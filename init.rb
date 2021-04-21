f3f9fe0087c792d7987fe4f7460e90770fff1adc
Liberatys <nick.flueckiger@renuo.ch>
2021-04-21 09:56:01 +0200

    Init commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :tracky do
+  name 'Tracky plugin'
+  author 'Nick Anthony Flueckiger'
+  description 'Time tracking plugin for redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'https://github.com/Liberty'
+end
