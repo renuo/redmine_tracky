a7f9a9d1f6373bf910ce1b007cb2d1b82b2a8baa
Liberatys <nick.flueckiger@renuo.ch>
2021-04-26 10:44:33 +0200

    Attempt to fix ci

--- test_setup.sh
+++ test_setup.sh
@@ -53,6 +53,8 @@ bundle exec rake redmine:plugins:migrate
 
 bundle exec rake db:structure:dump
 
+cd $PATH_TO_REDMINE
+
 # run tests
 # bundle exec rake TEST=test/unit/role_test.rb
 bundle exec rake redmine:plugins:test NAME=$NAME_OF_PLUGIN
