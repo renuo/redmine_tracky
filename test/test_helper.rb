c79725781968136c33ffbc9fa215cd82fb59a9ad
Liberatys <nick.flueckiger@renuo.ch>
2021-05-04 09:08:28 +0200

    Remove formater

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,5 @@
 require File.expand_path("#{File.dirname(__FILE__)}/../../../test/test_helper")
 
 require 'simplecov'
-require File.expand_path(File.dirname(__FILE__) + "/coverage/html_formatter")
-SimpleCov.formatter = Redmine::Coverage::HtmlFormatter
 SimpleCov.start 'rails'
 # TODO: uncomment after scaffold and setup => SimpleCov.minimum_coverage 100
