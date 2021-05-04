68e5eaf7c0c8e507e5ebb98d08c45dae9a96a02d
Nick Flückiger <nick.flueckiger@renuo.ch>
2021-05-04 10:30:18 +0200

    Implement test data and adding first displays (#10)

--- app/models/timer_session.rb
+++ app/models/timer_session.rb
@@ -1,4 +1,6 @@
 class TimerSession < ActiveRecord::Base
+  SECONDS_IN_HOUR = 3600
+
   has_many :timer_session_issues, dependent: :destroy
   has_many :timer_session_time_entries, dependent: :destroy
 
@@ -6,4 +8,12 @@ class TimerSession < ActiveRecord::Base
 
   has_many :issues, through: :timer_session_issues
   has_many :time_entries, through: :timer_session_time_entries
+
+  scope :active_session, -> (user_id) { where(user_id: user_id, timer_end: nil) }
+  
+  scope :finished_sessions, -> { where.not(timer_end: nil) }
+
+  def splittable_hours
+    @splittable_hours ||= ((self.timer_end - self.timer_start) / SECONDS_IN_HOUR)
+  end
 end
