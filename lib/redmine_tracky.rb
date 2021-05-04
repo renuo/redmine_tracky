2000c4135fc7f4cf6e067fa83c2e4631dc0a9130
Liberatys <nick.flueckiger@renuo.ch>
2021-05-04 08:49:18 +0200

    Fix issue with linting and missed array dec

--- lib/redmine_tracky.rb
+++ lib/redmine_tracky.rb
@@ -1,4 +1,6 @@
-%{services}.each do | folder |
+# frozen_string_literal: true
+
+%w[services].each do |folder|
   folder_path = File.dirname(__FILE__) + "/../app/#{folder}"
   ActiveSupport::Dependencies.autoload_path += [folder_path]
 end
