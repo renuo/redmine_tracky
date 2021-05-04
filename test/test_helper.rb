7791eb0b5299fa50f53f669fe86250b4711712f1
Nick Flückiger <nick.flueckiger@renuo.ch>
2021-05-04 14:24:51 +0200

    Adding tests for date format (#15)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,5 +4,20 @@
 require File.expand_path("#{File.dirname(__FILE__)}/../../../test/test_helper")
 
 require 'simplecov'
-SimpleCov.start 'rails'
-# TODO: uncomment after scaffold and setup => SimpleCov.minimum_coverage 100
+
+if Dir.pwd.match(/plugins\/redmine_tracky/)
+  covdir = 'coverage'
+else
+  covdir = 'plugins/redmine_tracky/coverage'
+end
+
+SimpleCov.coverage_dir(covdir)
+SimpleCov.start 'rails' do
+  add_filter do |source_file|
+    # only show files belonging to planner, except init.rb which is not fully testable
+    source_file.filename.match(/redmine_tracky/).nil? ||
+      !source_file.filename.match(/redmine_tracky\/init.rb/).nil?
+  end
+end
+
+SimpleCov.minimum_coverage 100
