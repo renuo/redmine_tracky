9e70dd0a23e2f5d37b109d48d663fe59f6d90624
Liberatys <nick.flueckiger@renuo.ch>
2021-05-04 17:03:28 +0200

    Adding icons and styling container

--- assets/javascripts/redmine_tracky.js
+++ assets/javascripts/redmine_tracky.js
@@ -1,9 +1,11 @@
 import './jquery.datetimepicker.full.js';
 import DateTimePickerRegister from './datetime_picker_register.js';
 import IssueListHandler from './issue_list_handler.js';
+import IssueCompleter from './issue_completer.js';
 
 DateTimePickerRegister.bind();
-
 IssueListHandler.bind();
+IssueCompleter.bind();
 // Export for use in reloaded partials
 window.IssueListHandler = IssueListHandler;
+window.IssueCompleter = IssueCompleter;
