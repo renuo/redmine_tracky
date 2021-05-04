dd9f801072cdeea89bc1c512ca0b5244653298bb
Liberatys <nick.flueckiger@renuo.ch>
2021-05-04 10:27:00 +0200

    Implement test data and adding first displays

--- app/controllers/timer_sessions_controller.rb
+++ app/controllers/timer_sessions_controller.rb
@@ -1,5 +1,61 @@
 class TimerSessionsController < ApplicationController
+  before_action :set_current_user
+  before_action :set_current_timer_session
 
   def index
+    create_test_data
+    @timer_sessions = TimerSession.includes(:issues).finished_sessions.where(user_id: @current_user)
+      .group_by { |entry| entry.timer_start.to_date }
+  end
+
+  def create_test_data
+    if TimerSession.count == 0
+      timer_sessions = [
+        TimerSession.create!(
+          timer_start: Time.zone.now - 1.hour, 
+          timer_end: Time.zone.now,
+          comments: 'Working on IPA',
+          user: @current_user,
+        ),
+        TimerSession.create!(
+          timer_start: Time.zone.now - 1.day - 1.hour, 
+          timer_end: Time.zone.now - 1.day,
+          comments: 'Working on LME',
+          user: @current_user
+        ),
+        TimerSession.create!(
+          timer_start: Time.zone.now - 1.hour, 
+          timer_end: Time.zone.now,
+          comments: 'Working on Game',
+          user: @current_user,
+        ),
+        TimerSession.create!(
+          timer_start: Time.zone.now - 1.day - 1.hour, 
+          timer_end: Time.zone.now - 1.day,
+          comments: 'Working on Not Game',
+          user: @current_user
+        )
+      ]
+
+      timer_sessions.each do | timer_session |
+        issues = Issue.where(id: [1, 2])
+        issues.each do | issue |
+          TimerSessionIssue.create!(
+            timer_session: timer_session,
+            issue: issue,
+          )
+        end
+      end
+    end
+  end
+
+  private
+
+  def set_current_user
+    @current_user = User.current
+  end
+
+  def set_current_timer_session
+    @current_timer_session = TimerSession.active_session(@current_user.id).first
   end
 end
