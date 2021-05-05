2526b414950b094082bfd3858dc82ca91cdba4fe
Nick Flückiger <nick.flueckiger@renuo.ch>
2021-05-05 11:51:52 +0200

    Add first input validation (#19)

--- app/models/timer_session.rb
+++ app/models/timer_session.rb
@@ -11,9 +11,13 @@ class TimerSession < RedmineTrackyApplicationRecord
   has_many :issues, through: :timer_session_issues
   has_many :time_entries, through: :timer_session_time_entries
 
+  validate :validate_session_attributes, on: :update
+
   scope :active_session, ->(user_id) { where(user_id: user_id, finished: false) }
 
   scope :finished_sessions, -> { where(finished: true) }
+
+  validate :start_before_end_date
 
   attr_accessor :issue_id
 
@@ -23,5 +27,35 @@ class TimerSession < RedmineTrackyApplicationRecord
 
   def session_finished?
     timer_end.present?
+  end
+
+  def start_and_end_present
+    errors.add(:timer_start, :blank) if timer_start.blank?
+    errors.add(:timer_end, :blank) if timer_end.blank?
+  end
+
+  def comment_present
+    # errors.add(:comments, :blank) if comments.blank?
+  end
+
+  def issues_selected
+    errors.add(:issue_id, :no_selection) if issues.count.zero?
+  end
+
+  def start_before_end_date
+    return if timer_start.blank? || timer_end.blank?
+
+    if timer_end < timer_start
+      errors.add(:timer_start, :after_end)
+      errors.add(:timer_end, :before_start)
+    end
+  end
+
+  def validate_session_attributes
+    if finished_changed?
+      start_and_end_present
+      comment_present
+      issues_selected
+    end
   end
 end
