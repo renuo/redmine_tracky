ea353030df3d00555538caded3703bcd00b4c05d
Simon Huber <simon.huber@renuo.ch>
2021-09-20 13:38:01 +0200

    Use Lambda for Time-Default (#51)

--- app/services/filtering/timer_sessions_filter.rb
+++ app/services/filtering/timer_sessions_filter.rb
@@ -4,8 +4,8 @@ module Filtering
   class TimerSessionsFilter < BaseFilter
     include Filtering::TimerSessions
 
-    attribute :min_date, :date, default: Time.current.to_date.at_beginning_of_week - 1.week
-    attribute :max_date, :date, default: Time.current.to_date.at_end_of_week
+    attribute :min_date, :date, default: -> { Time.current.to_date.at_beginning_of_week - 1.week }
+    attribute :max_date, :date, default: -> { Time.current.to_date.at_end_of_week }
 
     private
 
