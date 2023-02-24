8dffb2bb23ed5f8c2e0d67197523979b05ab2b06
Anes Hodza <anes.hodza@renuo.ch>
2023-02-24 12:25:40 +0100

    Fix specs

--- test/unit/settings_manager_test.rb
+++ test/unit/settings_manager_test.rb
@@ -22,15 +22,18 @@ class SettingsManagerTest < ActiveSupport::TestCase
   test '#max_hours_recorded_per_day - value' do
     Setting['plugin_redmine_tracky']['max_hours_recorded_per_day'] = '8'
     assert_equal SettingsManager.max_hours_recorded_per_day, 8
+    Setting['plugin_redmine_tracky']['max_hours_recorded_per_day'] = nil
   end
 
   test '#max_hours_recorded_per_session - value' do
     Setting['plugin_redmine_tracky']['max_hours_recorded_per_session'] = '8'
     assert_equal SettingsManager.max_hours_recorded_per_session, 8
+    Setting['plugin_redmine_tracky']['max_hours_recorded_per_session'] = nil
   end
 
   test '#min_hours_to_record - value' do
     Setting['plugin_redmine_tracky']['min_hours_to_record'] = '1'
     assert_equal SettingsManager.min_hours_to_record, 1
+    Setting['plugin_redmine_tracky']['min_hours_to_record'] = nil
   end
 end
