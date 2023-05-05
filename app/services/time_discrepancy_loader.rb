e08cce39b07107a1fee1eb59bb5efe45493063d6
Simon Isler <mail@simonisler.ch>
2023-05-05 14:42:08 +0200

    Fix `TimeDiscrepancyLoader` (#95)

--- app/services/time_discrepancy_loader.rb
+++ app/services/time_discrepancy_loader.rb
@@ -7,12 +7,26 @@ class TimeDiscrepancyLoader
   def self.uneven_timer_sessions(scope)
     scope.joins(:time_entries)
          .group(:id, :timer_start, :timer_end)
-         .having(<<~SQL)
-           CAST(SUM(time_entries.hours)
-           AS DECIMAL(10, #{DECIMALS_TO_ROUND_TO}))
-           !=
-           CAST(TIMESTAMPDIFF(SECOND, timer_sessions.timer_start, timer_sessions.timer_end) / 3600.0
-           AS DECIMAL(10, #{DECIMALS_TO_ROUND_TO}))
-         SQL
+         .having(
+           <<~SQL
+             CAST(SUM(time_entries.hours)
+             AS DECIMAL(10, #{DECIMALS_TO_ROUND_TO}))
+             !=
+             CAST((#{time_difference_expr}) / 3600.0
+             AS DECIMAL(10, #{DECIMALS_TO_ROUND_TO}))
+           SQL
+         )
+  end
+
+  def self.time_difference_expr
+    if using_postgresql?
+      'EXTRACT(EPOCH FROM timer_sessions.timer_end) - EXTRACT(EPOCH FROM timer_sessions.timer_start)'
+    else
+      'TIMESTAMPDIFF(SECOND, timer_sessions.timer_start, timer_sessions.timer_end)'
+    end
+  end
+
+  def self.using_postgresql?
+    ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
   end
 end
