2a9a7602762d90418f7ae000b69d14ac7d665686
simon <mail@simonisler.ch>
2023-09-15 16:56:55 +0200

    Order hits by project and subject differently

--- app/services/issue_searcher.rb
+++ app/services/issue_searcher.rb
@@ -19,21 +19,30 @@ class IssueSearcher
 
   def search_by_term(search_term, scope)
     found_issues = []
-    found_issues << scope.find_by(id: Regexp.last_match(1).to_i) if search_term =~ /\A#?(\d+)\z/
-    found_issues += projects_with_issues(search_term)
-    found_issues + issue_order(filter_closed_issues(scope.like(search_term))).limit(SEARCH_LIMIT)
+    found_issues << find_by_id(search_term, scope)
+    found_issues += hits_by_project(search_term)
+    found_issues + hits_by_subject(search_term, scope)
   end
 
-  def projects_with_issues(search_term)
+  def find_by_id(search_term, scope)
+    return unless search_term =~ /\A#?(\d+)\z/
+
+    scope.find_by(id: Regexp.last_match(1).to_i)
+  end
+
+  def hits_by_project(search_term)
     project_arel = Project.arel_table
-    issue_order(Issue.joins(:project).includes(:project, :tracker)
-                     .where(project_arel[:name].lower.matches("%#{search_term.downcase}%"))
-                     .open(TICKET_OPEN_STATUS))
-      .limit(PROJECT_RESULTS_LIMIT)
+    Issue.left_joins(:project, :time_entries)
+         .select('issues.*, COUNT(time_entries.id) as time_entries_count')
+         .group('issues.id')
+         .where(project_arel[:name].lower.matches("%#{search_term.downcase}%"))
+         .open(TICKET_OPEN_STATUS)
+         .order('time_entries_count DESC')
+         .limit(PROJECT_RESULTS_LIMIT)
   end
 
-  def issue_order(issue_query)
-    issue_query.order(id: :desc)
+  def hits_by_subject(search_term, scope)
+    filter_closed_issues(scope.like(search_term)).order(id: :desc).limit(SEARCH_LIMIT)
   end
 
   def filter_closed_issues(issues)
--- test/unit/issue_searcher_test.rb
+++ test/unit/issue_searcher_test.rb
@@ -32,7 +32,7 @@ class IssueSearcherTest < ActiveSupport::TestCase
 
   test 'call - search by project name' do
     search_term = @issues[0].project.name
-    assert_equal @issues[0], @service.call(search_term, Issue.all)[0]
+    assert_equal @issues[0], @service.call(search_term, Issue.all).last
   end
 
   test 'call - filters closed issues' do
@@ -45,5 +45,25 @@ class IssueSearcherTest < ActiveSupport::TestCase
 
     search_term = '100'
     assert_equal [@issues[0], second_issue], @service.call(search_term, Issue.all)
+  end
+
+  test 'call - hits by project name are ordered by time entries count (descending)' do
+    4.times { @issues[0].time_entries.create(hours: 1, user: User.current, spent_on: Date.yesterday) }
+    second_issue = FactoryBot.create(:issue, id: 104, subject: 'Very special Renuo issue', project: Project.first)
+    3.times { second_issue.time_entries.create!(hours: 1, user: User.current, spent_on: Date.yesterday) }
+
+    search_term = Project.first.name
+    actual_issues = @service.call(search_term, Issue.all)
+
+    assert_equal @issues[0], actual_issues[0]
+    assert_equal second_issue, actual_issues[1]
+  end
+
+  test 'call - hits by subject are ordered by id (descending)' do
+    search_term = 'special Renuo issue'
+    actual_issues = @service.call(search_term, Issue.all)
+
+    assert_equal @issues[1], actual_issues[0]
+    assert_equal @issues[0], actual_issues[1]
   end
 end
