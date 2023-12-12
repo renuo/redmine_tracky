51b70ec5bc0235674ec30301bb61a6896c3350c1
Chris Hunziker <chris.hunziker@renuo.ch>
2023-12-12 14:45:45 +0100

    Fix linter and specs

--- app/controllers/time_tracker_controller.rb
+++ app/controllers/time_tracker_controller.rb
@@ -1,76 +1,44 @@
 # frozen_string_literal: true
 
-# rubocop:disable Style/IfUnlessModifier, Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity, Metrics/AbcSize
 class TimeTrackerController < TrackyController
   def start
     # Early return if timer already running
-    if @current_timer_session
-      render :update, layout: false and return
-    end
+    return render(:update, layout: false) if @current_timer_session
 
     @current_timer_session = SessionCreator.new(@current_user, timer_params, params[:commit]).create
 
-    # Go to start page if timer session is not valid
-    unless @current_timer_session.valid?
-      @current_timer_session.errors.add(:base, :invalid)
-      render :start, layout: false and return
-    end
+    return render_start_page unless @current_timer_session.valid?
 
-    issue_connector = IssueConnector.new(timer_params[:issue_ids] || [], @current_timer_session)
-
-    # Go to update page if connected issues are not valid
-    unless issue_connector.run
-      @current_timer_session.errors.add(:issue_id, :invalid)
-      render :update, layout: false and return
-    end
+    return if issues_invalid
 
     # Early close the timer session if the timer_end is set and it's valid
-    if @current_timer_session.session_finished? && @current_timer_session.update(finished: true)
-      TimeSplitter.new(@current_timer_session, @current_timer_session.issues).create_time_entries
-      flash[:notice] = l(:notice_successful_update)
-      render :stop, layout: false and return
-    end
+    return render_stop_page if @current_timer_session.session_finished? &&
+                               @current_timer_session.update(finished: true)
 
-    # Go back to the start otherwise
     render :start, layout: false
   end
 
   def stop
-    if @current_timer_session.nil?
-      render :stop, layout: false and return
-    end
+    return render(:stop, layout: false) if @current_timer_session.nil?
 
-    if params[:cancel]
-      @current_timer_session.destroy
-      render :cancel, layout: false and return
-    end
+    return stop_timer unless params[:cancel]
 
-    if @current_timer_session.update(timer_end: default_end_time_for_timer(@current_timer_session), finished: true)
-      TimeSplitter.new(@current_timer_session, @current_timer_session.issues).create_time_entries
-      flash[:notice] = l(:notice_successful_update)
-      render :stop, layout: false
-    else
-      render :update, layout: false
-    end
+    @current_timer_session.destroy
+    render :cancel, layout: false
   end
 
   def update
-    # Start over if timer is not running
-    if @current_timer_session.nil?
-      render :start, layout: false and return
-    end
+    return render(:start, layout: false) if @current_timer_session.nil?
 
-    if @current_timer_session.update(timer_params)
-      head :no_content
-    else
-      render :update, layout: false
-    end
+    return head(:no_content) if @current_timer_session.update(timer_params)
+
+    render :update, layout: false
   end
 
   private
 
   def default_end_time_for_timer(timer_session)
-    (timer_session&.timer_end.presence || timer_params[:timer_end]&.presence || user_time_zone.now.asctime)
+    timer_session&.timer_end.presence || timer_params[:timer_end]&.presence || user_time_zone.now.asctime
   end
 
   def timer_params
@@ -82,5 +50,36 @@ class TimeTrackerController < TrackyController
       param[:issue_ids] ||= []
     end
   end
+
+  # Go to start page if timer session is not valid
+  def render_start_page
+    @current_timer_session.errors.add(:base, :invalid)
+    render :start, layout: false
+  end
+
+  def render_stop_page
+    TimeSplitter.new(@current_timer_session, @current_timer_session.issues).create_time_entries
+    flash[:notice] = l(:notice_successful_update)
+    render :stop, layout: false
+  end
+
+  # Go to update page if connected issues are not valid
+  def issues_invalid
+    issue_connector = IssueConnector.new(timer_params[:issue_ids] || [], @current_timer_session)
+
+    return if issue_connector.run
+
+    @current_timer_session.errors.add(:issue_id, :invalid)
+    render :update, layout: false
+  end
+
+  def stop_timer
+    if @current_timer_session.update(timer_end: default_end_time_for_timer(@current_timer_session), finished: true)
+      TimeSplitter.new(@current_timer_session, @current_timer_session.issues).create_time_entries
+      flash[:notice] = l(:notice_successful_update)
+      return render :stop, layout: false
+    end
+
+    render :update, layout: false
+  end
 end
-# rubocop:enable Style/IfUnlessModifier, Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity, Metrics/AbcSize
