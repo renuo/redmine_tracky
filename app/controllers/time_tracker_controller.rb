e2185713fb3432b846dd5a6de19224958ab14cfd
Daniel Bengl <daniel.bengl@renuo.ch>
2024-06-12 15:19:39 +0200

    Prototype TimeTracker CRUD

--- app/controllers/time_tracker_controller.rb
+++ app/controllers/time_tracker_controller.rb
@@ -3,20 +3,66 @@
 # rubocop:disable Metrics/AbcSize
 # rubocop:disable Metrics/MethodLength
 class TimeTrackerController < TrackyController
-  before_action :set_current_timer_session, only: %i[create_or_update cancel]
+  before_action :set_current_timer_session, only: %i[create update destroy]
 
-  def create_or_update
-    return start_timer unless @current_timer_session
+  def create
+    # don't allow to start a new timer if there is already an active one
+    if @current_timer_session.present?
+      head :conflict and return
+    end
 
-    cancel_timer if params[:cancel].present?
-    stop_timer if timer_params[:timer_end].present?
+    @current_timer_session = SessionCreator.new(User.current, timer_params, params[:commit]).create
+
+    
+
+    create_params = timer_params
+
+    create_params[:start_time] = if params[:commit] == 'start_from_previous'
+                                   start_time_from_last_session(@current_timer_session)
+                                 else
+                                   default_start_time_for_timer(@current_timer_session)
+                                 end
+
+    session = TimerSession.create(create_params.merge(finished: create_params[:end].present?))
+
+    unless timer_session.valid?
+      @current_timer_session.errors.add(:base, :invalid)
+      render_js :start and return
+    end
+
+
+    unless session.valid?
+      render :new
+    else
+      render :created
+    end
+
+
+    issue_connector = IssueConnector.new(timer_params[:issue_ids] || [], @current_timer_session)
+
+    unless issue_connector.run
+      @current_timer_session.errors.add(:issue_id, :invalid)
+      render_js :update and return
+    end
   end
 
-  def cancel
-    if @current_timer_session.blank?
-      head :not_found
+  def update
+    render :not_found and return if @current_timer_session.blank?
+
+    if @current_timer_session.update(timer_params)
+      render :updated
     else
-      cancel_timer
+      render :update
+    end
+  end
+
+  def destroy
+    render :not_found and return if @current_timer_session.blank?
+
+    if @current_timer_session.destroy
+      render_js :cancel
+    else
+      render_js :cancel, :unprocessable_entity
     end
   end
 
@@ -24,14 +70,6 @@ class TimeTrackerController < TrackyController
 
   def set_current_timer_session
     @current_timer_session = TimerSession.active.find_by(user: User.current)
-  end
-
-  def cancel_timer
-    if @current_timer_session.destroy
-      render_js :cancel
-    else
-      render_js :cancel, :unprocessable_entity
-    end
   end
 
   # rubocop:disable Metrics/CyclomaticComplexity
@@ -75,6 +113,14 @@ class TimeTrackerController < TrackyController
     else
       render_js :update, :unprocessable_entity
     end
+  end
+
+  def default_start_time_for_timer(timer_session)
+    timer_session&.timer_start.presence || timer_params[:timer_start]&.presence || user_time_zone.now.asctime
+  end
+
+  def start_time_from_last_session(_timer_session)
+    raise 'Not implenmented yet!'
   end
 
   def default_end_time_for_timer(timer_session)
