dbe7779f3bfed59f6cf2668273c9c1405bb6b92b
Daniel Bengl <daniel.bengl@renuo.ch>
2024-06-25 17:30:53 +0200

    Improve coverage

--- test/functional/time_tracker_controller_test.rb
+++ test/functional/time_tracker_controller_test.rb
@@ -77,9 +77,8 @@ class TimeTrackerControllerTest < ActionController::TestCase
   test '#create - with existing session' do
   FactoryBot.create(:timer_session, user: User.find(1), finished: false)
     assert_equal 1, TimerSession.count
-    recorded_time = Time.zone.now - 1.hour
     post :create, params: { timer_session: {
-      timer_start: recorded_time,
+      timer_start: Time.zone.now - 1.hour,
       timer_end: Time.zone.now,
       comments: 'Starting a session with conflict',
       issue_ids: ['1']
@@ -91,15 +90,30 @@ class TimeTrackerControllerTest < ActionController::TestCase
 
   test '#create - with invalid params' do
     assert_equal 0, TimerSession.count
-    recorded_time = Time.zone.now + 1.hour
     post :create, params: { timer_session: {
-      timer_start: recorded_time,
+      timer_start: Time.zone.now + 1.hour,
       timer_end: Time.zone.now,
       comments: 'Starting a new session',
       issue_ids: ['1']
     } }, xhr: true
     assert_equal 0, TimerSession.count
     assert_response 422
+  end
+
+  test '#create - from last session' do
+    FactoryBot.create(:timer_session, user: User.find(1), finished: true, timer_start: Time.zone.now - 2.hours, timer_end: Time.zone.now - 1.hour)
+    assert_equal 1, TimerSession.count
+    post :create, params: { timer_session: {
+      timer_start: nil,
+      timer_end: Time.zone.now + 1.hour,
+      comments: 'Starting a new session',
+      issue_ids: ['1'],
+      commit: 'continue'
+    } }, xhr: true
+    assert_equal 2, TimerSession.count
+    assert_equal TimerSession.last.finished, true
+    assert_in_delta TimerSession.last.timer_start, Time.zone.now - 1.hour, 1.second
+    assert_response 200
   end
 
   test '#update - with end time' do
