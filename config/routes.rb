853fb89502a8df1b2b2e5d71005ebeae5084b91f
Daniel Bengl <daniel.bengl@renuo.ch>
2024-06-25 16:35:05 +0200

    Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@
 resources :timer_sessions, only: %i[index edit update patch destroy]
 
 get 'timer_sessions_report', to: 'timer_sessions#report', as: :timer_sessions_report
-get 'timer_sessions_time_error', to: 'timer_sessions#time_error', as: :timer_sessions_time_error
+get 'timer_sessions_time_error/:id', to: 'timer_sessions#time_error', as: :timer_sessions_time_error
 post 'timer_sessions_rebalance/:id', to: 'timer_sessions#rebalance', as: :timer_sessions_rebalance
 post 'timer_sessions_continue/:id', to: 'timer_sessions#continue', as: :timer_sessions_continue
 
