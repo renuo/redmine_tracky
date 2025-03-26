b24ff3379d72e0adb5315690cbd349cb26a40c69
Daniel Bengl <53896675+CuddlyBunion341@users.noreply.github.com>
2025-03-26 08:36:16 +0100

    Update application_system_test_case.rb

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,6 +3,8 @@
 require File.expand_path('test_helper', __dir__)
 require File.expand_path('../../../test/application_system_test_case', __dir__)
 
+# borrowed and adapted from Redmine application system test case. The url assertion was a source of timing problems.
+# https://github.com/redmine/redmine/blob/06bbaebef8366bf19f73da0bf4e1315d23dc4697/test/application_system_test_case.rb#L69-L78
 def login_user(login, password)
   visit '/my/page'
   assert_equal '/login', current_path
