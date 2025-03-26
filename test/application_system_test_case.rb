f642660d18a2ac5a60e40286920fab31d7bbf750
Daniel Bengl <53896675+CuddlyBunion341@users.noreply.github.com>
2025-03-26 08:44:45 +0100

    Fix workflow (#167)

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,3 +2,17 @@
 
 require File.expand_path('test_helper', __dir__)
 require File.expand_path('../../../test/application_system_test_case', __dir__)
+
+# borrowed and adapted from Redmine application system test case. The url assertion was a source of timing problems.
+# https://github.com/redmine/redmine/blob/06bbaebef8366bf19f73da0bf4e1315d23dc4697/test/application_system_test_case.rb#L69-L78
+def login_user(login, password)
+  visit '/my/page'
+  assert_equal '/login', current_path
+  within('#login-form form') do
+    fill_in 'username', with: login
+    fill_in 'password', with: password
+    find('input[name=login]').click
+  end
+  assert has_content?(I18n.t('label_my_account'))
+  assert_equal '/my/page', current_path
+end
