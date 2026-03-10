4effe601f25853bacbb95cf162745692e305f232
Alessandro Rodi <alessandro.rodi@renuo.ch>
2026-03-10 15:57:02 +0100

    patch does not exist

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.htm
-resources :timer_sessions, only: %i[index edit update patch destroy]
+resources :timer_sessions, only: %i[index edit update destroy]
 
 get 'timer_sessions_report', to: 'timer_sessions#report', as: :timer_sessions_report
 get 'timer_sessions_time_error/:id', to: 'timer_sessions#time_error', as: :timer_sessions_time_error
