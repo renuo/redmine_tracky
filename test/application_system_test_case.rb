d8d32b19254a2fbd07a7367487dca3b92c381da0
Oliver Thoma <oliver.thoma@hey.com>
2026-07-06 16:05:59 +0200

    Fix system specs

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,7 +3,11 @@
 require File.expand_path('test_helper', __dir__)
 require File.expand_path('../../../test/application_system_test_case', __dir__)
 
-# borrowed and adapted from Redmine application system test case. The url assertion was a source of timing problems.
+# Redmine core dropped this from its own ApplicationSystemTestCase (redmine/redmine@19a0472, 2026-06-22),
+# which lets Rails default to multiple parallel workers and makes every system test collide on one Capybara session.
+ApplicationSystemTestCase.parallelize(workers: 1)
+
+# borrowed and adapted from Redmine application system test case.
 # https://github.com/redmine/redmine/blob/06bbaebef8366bf19f73da0bf4e1315d23dc4697/test/application_system_test_case.rb#L69-L78
 def login_user(login, password)
   visit '/my/page'
